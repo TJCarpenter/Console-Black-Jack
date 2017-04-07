235d806505b9e2987705a0ce9d6510f6f36652ad
Tyler Carpenter <tjcarpenter15@my.trine.edu>
2017-04-06 22:44:54 -0400

    Update CardDraw.cpp

--- CardDraw.cpp
+++ CardDraw.cpp
@@ -1,101 +1,46 @@
 #include "stdafx.h"
-#include "CardDraw.h"
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+#include <random>
+#include <algorithm>   
+#include <time.h>
+#include "Card.h"
+
+#define TestPrintDeck
 
 using namespace std;
 
-class card {
-public:
-	string getSuit();
-	string getValue();
-	string getFullCard();
-	string concatenate(string, string);
-	card(string, string);
-private:
-	string suit;
-	string value;
-	string fullCard;
-};
-
-void printCard(card);
-int checkCard(vector <string>, card);
-
-//array of the cards suits and value
-string suits[4] = { "S", "C", "H", "D" };
-string value[13] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-
-vector <string> drawnCards;
-
-
-//draws the card
-void CardDraw(int numDraw) 
+void card::createDeck()
 {
-	int j = 1, decks = 1;
-	drawnCards.resize(1);
-	while (j <= numDraw)
+	string cardSuits[4] = { "S", "C", "H", "D" };
+	string cardValue[13] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+	for (int s = 0; s < 4; s++)
 	{
-		randomCard:
-		card c(suits[rand() % 4], value[rand() % 13]);
-		for (int i = 0; i < drawnCards.size(); i++)
+		for (int v = 0; v < 13; v++)
 		{
-			c.concatenate(c.getSuit(), c.getValue());
-			int check = checkCard(drawnCards, c);
-			if (check == 0)
-			{
-				goto randomCard;
-			}//end if
-			else
-			{
-				drawnCards.push_back(c.getFullCard());
-				printCard(c);
-				j++;
-				break;
-			}//end else
-		}//end for
-	}//end while
-}//end CardDraw
-
-//gets suit value
-string card::getSuit() {
-	return suit;
-}//end getSuit
-
-//gets card value
-string card::getValue() {
-	return value;
-}//end getValue
-
-//gets full card value and suit
-string card::getFullCard() {
-	return fullCard;
-}//end getFullCard
-
-//sets card values
-card::card(string s, string v) :suit(s), value(v)
-{}
-
-//concatenates into a single string
-string card::concatenate(string suit, string value) {
-	fullCard = value + suit;
-	return fullCard;
-}//end concatenate
-
-//prints card
-void printCard(card c) {
-	cout <<c.getFullCard() << endl;
-}//end printCard
-
-int checkCard(vector <string> v, card c) {
-	for (int i = 0; i < v.size(); i++) {
-		if (c.concatenate(c.getSuit(), c.getValue()) == drawnCards[i]) {
-			return 0;
-			break;
+			deck.push_back(cardSuits[s] + cardValue[v]);
 		}
-		else {
-			continue;
-		}
-		return 1;
 	}
+#ifdef TestPrintDeck
+	for (int i = 0; i < 52; i++)
+	{
+		cout << deck[i] << " ";
+	}
+#endif // TestPrintDeck
+
+}
+
+void card::shuffleDeck()
+{
+	mt19937 g(static_cast<uint32_t>(time(0)));
+	shuffle(deck.begin(), deck.end(), g);
+
+#ifdef TestPrintDeck
+	cout << endl;
+	for (int i = 0; i < 52; i++)
+	{
+		cout << deck[i] << " ";
+	}
+#endif // TestPrintDeck
 }

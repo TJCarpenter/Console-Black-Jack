5121d300a98b432ae58fbe3e48a6165379189bf4
Tyler Carpenter <tjcarpenter15@my.trine.edu>
2017-04-06 22:45:33 -0400

    Update CardDraw.h

--- CardDraw.h
+++ CardDraw.h
@@ -1,7 +1,35 @@
 #pragma once
-#ifndef CardDraw_h
-#define CardDraw_h
+#ifndef Card_h
+#define Card_h
 
-void CardDraw(int);
+#include <vector>
+#include <string>
+#include <cstdlib>
 
-#endif // !CardDraw_h
+using namespace std;
+
+class card
+{
+public:
+	void createDeck();
+	void shuffleDeck();
+	void pushBack(string s, string v)
+	{
+		deck.push_back(s + v);
+	}
+	string getCard()
+	{
+		return deck[numCardInDeck++];
+	}
+	int randomNumber()
+	{
+		return rand() % 1000;
+	}
+protected:
+	vector<string> deck;
+	int numCardInDeck = 0;
+};
+
+
+
+#endif // Card_h
